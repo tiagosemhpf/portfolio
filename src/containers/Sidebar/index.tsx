24e7aa18832635d0234e69bcf94bc50d6f6c3455
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-08-30 13:09:58 -0300

    Crie os componentes para textos

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+    <aside>
+        <img src="https://github.com/tiagosemhpf.png" />
+        <Titulo fontSize={20}>Tiago Pereira</Titulo>
+    </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+    <section>
+        <Titulo fontSize={16}>Sobre mim</Titulo>
+        <Paragrafo tipo="secundario">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </Paragrafo>
+    </section>
+)
+
+export default Sobre
