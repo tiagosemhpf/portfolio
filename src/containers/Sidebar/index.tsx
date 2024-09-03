c1c2557e698f6f87e1fd4a9b1f6cc66afcc2393d
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-03 13:40:54 -0300

    Estilize a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import Avatar from '../Avatar'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
     <aside>
-        <img src="https://github.com/tiagosemhpf.png" />
-        <Titulo fontSize={20}>Tiago Pereira</Titulo>
+        <SidebarContainer>
+            <Avatar />
+            <Titulo fontSize={20}>Tiago Pereira</Titulo>
+            <Paragrafo tipo="secundario" fontSize={16}>
+                tiagosemhpf
+            </Paragrafo>
+            <Descricao tipo="principal" fontSize={12}>
+                Dev Full Stack Python
+            </Descricao>
+            <BotaoTema>Trocar tema</BotaoTema>
+        </SidebarContainer>
     </aside>
 )
 
